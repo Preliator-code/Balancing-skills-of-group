8189f74eeabf7ea9946316faf0c905e66b5d7971
kevinC <kevin.cariou@transdev.fr>
2021-05-10 14:43:29 +0200

    Correction du bug qui me renvoyait un jsonCombinationRetainedExtra vide

--- js/calculWithCombinations.js
+++ js/calculWithCombinations.js
@@ -91,15 +91,14 @@ function getQuantileAndFilter(){
 		jsonElement.contenant.forEach(personne =>{
 				personOnRetained.push(personne)
 		})
-	})
-	
+	})	
 
 	if (dataTab.length % nombrePersonnes !== 0) {
 		quart1Extra = ss.quantile(scoreCombinationExtra, 0.47);
 		quart3Extra = ss.quantile(scoreCombinationExtra, 0.53);
 		noteMaxExtra = ss.max(scoreCombinationExtra);
 		jsonCombinationExtra.forEach(jsonElement =>{
-			if (jsonElement.scoreCombinaison > quart1Extra && jsonElement.scoreCombinaison < quart3Extra) {
+			if (jsonElement.scoreCombinaison >= quart1Extra && jsonElement.scoreCombinaison <= quart3Extra) {
 				jsonCombinationRetainedExtra.push(jsonElement)
 			}
 		})
